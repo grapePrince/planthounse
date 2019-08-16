7f3452f6a358de0ca4c3f06f952219414cfb48bb
grapePrince <tul90@naver.com>
2019-08-16 18:12:20 +0900

    검색 결과 만드는 중

--- site/js/main.js
+++ site/js/main.js
@@ -1,4 +1,5 @@
 $(function() {
+
   var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
@@ -6,4 +7,22 @@ $(function() {
     },
     loop: true
   });
+
+
+  $('.header__upper__search_btn').on('click', function() {
+    $('body').addClass('js-search-opened');
+  })
+
+  $('.layer_search__back').on('click', function() {
+    $('body').removeClass('js-search-opened');
+    $('.layer_search').removeClass('js-search-result js-search-result--list');
+  });
+
+  $('.layer_search__input__search, .layer_search__beforelist__item').on('click', function(e) {
+    e.preventDefault();
+    $('.layer_search').addClass('js-search-result js-search-result--list');
+  });
+
+  $( "#layer_search__result__sort" ).selectmenu();
+
 });
