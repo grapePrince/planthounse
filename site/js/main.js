cb1f127c1d62753c318d4cc65f7201db1591ca1b
grapePrince <tul90@naver.com>
2019-08-18 22:59:47 +0900

    검색결과까지 완료

--- site/js/main.js
+++ site/js/main.js
@@ -11,16 +11,28 @@ $(function() {
 
   $('.header__upper__search_btn').on('click', function() {
     $('body').addClass('js-search-opened');
+    $('.layer_search').addClass('js-search--search');
   })
 
   $('.layer_search__back').on('click', function() {
     $('body').removeClass('js-search-opened');
-    $('.layer_search').removeClass('js-search-result js-search-result--list');
+    $('.layer_search').removeClass('js-search--search js-search--result--1line js-search--result--2column');
   });
 
   $('.layer_search__input__search, .layer_search__beforelist__item').on('click', function(e) {
     e.preventDefault();
-    $('.layer_search').addClass('js-search-result js-search-result--list');
+    $('.layer_search').removeClass('js-search--search');
+    $('.layer_search').addClass('js-search--result--1line');
+  });
+
+  $('.layer_search__result__view--list').on('click', function() {
+    $('.layer_search').removeClass('js-search--result--2column');
+    $('.layer_search').addClass('js-search--result--1line');
+  });
+
+  $('.layer_search__result__view--thumb').on('click', function() {
+    $('.layer_search').removeClass('js-search--result--1line');
+    $('.layer_search').addClass('js-search--result--2column');
   });
 
   $( "#layer_search__result__sort" ).selectmenu();
